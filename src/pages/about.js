140fcdc00f7ef3c292ee087443a8ede847c35f53
katsuyaw <kw@kmail.me>
2021-04-17 14:17:16 -0700

    Format CSS

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,10 @@ import React from 'react';
 function About() {
     return (
         <>
-            <div className="font-light text-lg sm:text-xl md:text-2xl lg:text-3xl  mt-14 mx-14 sm:mt-24 sm:mx-20 md:mx-24 lg:mx-28 mb-20">
-                <p className="body-content">
-                    I love learning new things. <br />
+            <div className="content-container">
+                <div className="font-light text-lg sm:text-xl md:text-2xl lg:text-3xl  mt-14 mx-14 sm:mt-24 sm:mx-20 md:mx-24 lg:mx-28 mb-20">
+                    <p className="body-content">
+                        I love learning new things. <br />
                 I grew up in Japan and I was a pre-med student before I came to the US.
                 I learned C/C++, Java, HTML/CSS since I came here and I am currently learning React.
                 <br />I enjoy programming since it empowers me to build my ideas.
@@ -19,21 +20,9 @@ function About() {
                 Since the day I learned that I fell in love with the process.
                 {/* And I really enjoy what I am doing :) */}
 
-                </p>
+                    </p>
+                </div>
             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
         </>
     );
 };
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Projects() {
     return (
         <>
-
+            <div className="content-container">
+                <div className="body-container">
+                    <div className="body-content">
+                        Projects
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
