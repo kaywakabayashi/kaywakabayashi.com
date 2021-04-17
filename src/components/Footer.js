def27dbadf302da7abacf02c16d0ada4aa9d2d48
katsuyaw <kw@kmail.me>
2021-04-16 22:40:49 -0700

    Add tailwind

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="flex justify-center items-center h-16 bg-black text-white"></div>
+    )
+}
+
+export default Footer
