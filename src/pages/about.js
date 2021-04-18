91fa7313ae665d8f9681f0653b6af0ca3051d82e
katsuyaw <kw@kmail.me>
2021-04-18 00:46:14 -0700

    Change spacing

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,8 @@ function About() {
     return (
         <>
             <div className="h-full grid justify-center">
-                <div className="font-light text-lg sm:text-xl md:text-2xl lg:text-3xl  mt-14 mx-14 sm:mt-24 sm:mx-20 md:mx-24 lg:mx-28 mb-20">
-                    <p className="body-content">
-                        I love learning new things. <br />
+                <div className="text-base tracking-wider leading-relax sm:tracking-wider sm:leading-relax mx-10 mt-16 sm:text-xl">
+                    I love learning new things. <br />
                         I was born and raised in Japan and I spent 4 years as a pre-med student before I came to the US.
 I wrote my first program in 2020 and so far I have learned C/C++, Java, HTML/CSS and I am learning ReactJS.
 
@@ -20,7 +19,6 @@ I wrote my first program in 2020 and so far I have learned C/C++, Java, HTML/CSS
                 Since the day I learned that I fell in love with the process.
                 {/* And I really enjoy what I am doing :) */}
 
-                    </p>
                 </div>
             </div>
         </>
