82be324fee0c59b16a2b3941487bea6a1c7a9f31
katsuyaw <kw@kmail.me>
2021-04-18 15:51:42 -0700

    Fix margin

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <>
             <div className="h-full grid justify-center">
-                <div className="text-base tracking-wider leading-relax sm:tracking-wider sm:leading-relax mx-12 sm:mx-36 mt-16 sm:text-xl">
+                <div className="text-base tracking-wider leading-relax sm:tracking-wider sm:leading-relax mx-12 sm:mx-56 mt-16 sm:text-xl">
                     I love learning new things. <br />
                         I was born and raised in Japan and I spent 4 years as a pre-med student before I came to the US.
 I wrote my first program in 2020 and so far I have learned C/C++, Java, HTML/CSS and I am learning ReactJS.
