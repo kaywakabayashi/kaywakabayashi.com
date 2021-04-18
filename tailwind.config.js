5c321c1f37109651ea4e464999c185c3cc286723
katsuyaw <kw@kmail.me>
2021-04-18 16:38:32 -0700

    Add Helvetica Neue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'display': ['Oswald'],
-      'body': ['Open Sans']
-    }
+      sans: [
+        '"Inter"',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
 
   },
   variants: {
