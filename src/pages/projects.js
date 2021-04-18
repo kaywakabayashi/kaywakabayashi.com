6b3c107795f1c66fbc27b25c867772a3976b6918
katsuyaw <kw@kmail.me>
2021-04-18 15:50:26 -0700

    Remove comments

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -80,75 +80,7 @@ function Projects() {
                     <a className="text-blue-500" href="https://efficiently.app">efficiently.app</a>
                 </div>
                 <p className="mt-12 text-center text-base mx-8 leading-loose">Multi Platform React Todo App Project. Comming soon.</p>
-
             </div>
-
-
-
-            {/* <h1 className="font-semibold text-center mb-14">THE R PROJECTS</h1>
-            <div className="flex mb-10">
-                <div className="relative font-bold text-xl transform -rotate-90">2020</div>
-               
-            </div>
-            <p className="text-base text-center mb-5">Music production project. Music video shot in LA. Available on streaming services.</p>
-            <div className="text-center text-sm text-blue-500">
-                <a className="" href="https://music.apple.com/us/artist/young-wolf/1511812234">Apple Music</a>
-                <span className="text-black" > / </span>
-                <a className="" href="https://open.spotify.com/album/5yCCUAAUxjaUnfWBML4evc?si=_d1iwwvyT8qz2QTgey0XmQ">Spotify</a>
-                <span className="text-black" > / </span>
-                <a className="" href="https://www.amazon.com/DROP-Explicit-Young-Wolf/dp/B087QN7PQ5">Amazon</a>
-            </div>
-            <div className="content-container">
-                <div className="body-container">
-                    <div className="body-content">
-                        <div className="items-center mb-40">
-                            <h1 className="font-semibold text-center mb-14">THE R PROJECTS</h1>
-                            <div className="flex mb-10">
-                                <div className="font-bold text-xl transform -rotate-90">2020</div>
-                                <div className="items-center"><iframe src="https://player.vimeo.com/video/538200578?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="320" height="230" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="D R O P   I T !.mp4"></iframe></div>
-                            </div>
-                            <p className="text-base text-center mb-5">Music production project. Music video shot in LA. Available on streaming services.</p>
-                            <div className="text-center text-sm text-blue-500">
-                                <a className="" href="https://music.apple.com/us/artist/young-wolf/1511812234">Apple Music</a>
-                                <span className="text-black" > / </span>
-                                <a className="" href="https://open.spotify.com/album/5yCCUAAUxjaUnfWBML4evc?si=_d1iwwvyT8qz2QTgey0XmQ">Spotify</a>
-                                <span className="text-black" > / </span>
-                                <a className="" href="https://www.amazon.com/DROP-Explicit-Young-Wolf/dp/B087QN7PQ5">Amazon</a>
-                            </div>
-                        </div>
-
-
-                        <div className="mb-40">
-                            <h1 className="font-semibold text-center">Let's Trade</h1>
-                            <div className="mb-20">
-                                <div className="flex mb-10">
-                                    <div className="font-bold text-xl transform -rotate-90 ml-40">2021</div>
-                                    <div className="items-center"><img src={LetsTradeScreen} /></div>
-                                </div>
-                                <div className="text-center"><p>Virtual stock trading game project. Implemented in C++.</p></div>
-                            </div>
-                        </div>
-
-                        <div className="mb-40">
-                            <h1 className="font-semibold text-center mb-14">ETERNAL CARD</h1>
-                            <div className="mb-20">
-                                <div className="flex mb-10">
-                                    <div className="font-bold text-xl transform -rotate-90 ml-40">2021</div>
-                                    <div className="items-center"><iframe src="https://player.vimeo.com/video/538203679?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="3D Animation.mp4"></iframe></div>
-                                </div>
-                                <div className="text-center"><p>NFT(non-fungible token) project. Design an NFT product and sold on the market.</p></div>
-                            </div>
-                        </div>
-
-
-
-
-
-
-
-                    </div>
-                </div>
-            </div> */}
         </>
     );
 };
