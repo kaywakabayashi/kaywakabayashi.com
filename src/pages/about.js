470617bf803e72a420e78879a65731bbd3ef81af
katsuyaw <kw@kmail.me>
2021-04-18 00:18:28 -0700

    Fix format

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,17 +3,17 @@ import React from 'react';
 function About() {
     return (
         <>
-            <div className="content-container">
+            <div className="h-full grid justify-center">
                 <div className="font-light text-lg sm:text-xl md:text-2xl lg:text-3xl  mt-14 mx-14 sm:mt-24 sm:mx-20 md:mx-24 lg:mx-28 mb-20">
                     <p className="body-content">
                         I love learning new things. <br />
-                I grew up in Japan and I was a pre-med student before I came to the US.
-                I learned C/C++, Java, HTML/CSS since I came here and I am currently learning React.
-                <br />I enjoy programming since it empowers me to build my ideas.
+                        I was born and raised in Japan and I spent 4 years as a pre-med student before I came to the US.
+I wrote my first program in 2020 and so far I have learned C/C++, Java, HTML/CSS and I am learning ReactJS.
 
-                <br />
+                <br /><br />
 
-                I also enjoy designing. <br />
+                I enjoy designing. <br />
+                I have done multiple creative projects.
                 I believe in the power of minimalism. Making a good
                 elaborate design is difficult but making it simple
                 is much more difficult. <br />
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,41 @@ import { Link } from 'react-router-dom'
 function Home() {
     return (
         <>
-            <div>
-                <div className="content-container">
-                    <div className="body-container">
-                        <p className="body-content">
+            <div className="h-full grid justify-center">
+                <div className="text-base tracking-wider leading-relax sm:tracking-wider sm:leading-relax mx-10 mt-3 sm:mt-8 sm:text-xl">
+                    <p >Hello there,</p><br />My name is
+                    <Link to="/about" className="pulse-blue"> Katsuya Wakabayashi. </Link><br />
+                    I
+                    <a className="pulse-green" href="https://github.com/katsuyaw"> code</a>, write, and
+                            <Link to="/projects" className="pulse-yellow"> create new value</Link>.
+                            <br />I am currently a CS major college student in LA and
+                            looking for <a className="pulse-pink" href="https://linkedin.com/in/kats"> internships</a>. <br />
+                            Looking forward to working with you.
+
+
+                            <p className="leading-relaxed tracking-wide"><br />
+                        <span>Sent from my </span>
+                        <a className="pulse-purple" href="tel:334-454-3210">iPhone</a>
+                    </p>
+                    <div className="flex justify-end mt-0 sm:mt-20">
+                        <div className="">
+                            <a href="https://drive.google.com/file/d/1hZVi1QKysHm6gSdVz0uJRsUPZbHabA3n/view?usp=sharing" className="mx-4 px-4 py-4 action-btn" >
+                                <svg className="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clipRule="evenodd" /></svg>
+                            </a>
+                        </div>
+                        <div className="">
+                            <a className="mx-4 px-4 py-4 action-btn" href="mailto:reply@katsuya.co?subject=Re: [Hi there]">
+                                <div><svg className="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" /></svg></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            {/* <div>
+                <div className="">
+                    <div className="">
+                        <p className="">
                             <p className="mb-10">Hello there,</p>
                             My name is
                             <Link to="/about" className="pulse-blue"> Katsuya Wakabayashi. </Link><br />
@@ -22,7 +53,7 @@ function Home() {
                             <span>Sent from my </span>
                             <a className="pulse-purple" href="tel:334-454-3210">iPhone</a>
                         </p>
-                        <div className="absolute bottom-0 right-4 ">
+                        <div className="absolute bottom-0">
                             <div className="inline-block">
                                 <a href="https://drive.google.com/file/d/1hZVi1QKysHm6gSdVz0uJRsUPZbHabA3n/view?usp=sharing" className="action-btn" >
                                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clipRule="evenodd" /></svg>
@@ -38,7 +69,7 @@ function Home() {
                     </div>
                 </div>
 
-            </div>
+            </div> */}
         </>
     );
 };
