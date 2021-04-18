8224f62b254d709f964ff868952573eb70e20ebf
katsuyaw <kw@kmail.me>
2021-04-18 01:59:34 -0700

    Edit email

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ function Home() {
                             </a>
                         </div>
                         <div className="">
-                            <a className="mx-4 p-2 action-btn" href="mailto:reply@katsuya.co?subject=Re: [Hi there]">
+                            <a className="mx-4 p-2 action-btn" href="mailto:reply@katsuya.me?subject=Re: [Hi there]">
                                 <div><svg className="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" /></svg></div>
                             </a>
                         </div>
