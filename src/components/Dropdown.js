7ef44ec09b2ca1b2944e46ff4db8552028792bf6
katsuyaw <kw@kmail.me>
2021-04-18 00:26:07 -0700

    Fix spacing

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -20,7 +20,7 @@ const Dropdown = ({ isOpen, toggle, themeToggler }) => {
                     </Link>
                 </div>
 
-                <div className="action-btn p-2">
+                <div className="action-btn p-2 ml-4">
                     <Link className="" onClick={themeToggler}>
                         <svg className="w-5 h-5 sm:w-6 sm:h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" /></svg>
                     </Link>
