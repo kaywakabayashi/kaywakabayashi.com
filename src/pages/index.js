e8b756c07b61f542b515f78c2608194f5b132d1d
katsuyaw <kw@kmail.me>
2021-04-17 20:34:04 -0700

    Add resume link

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,9 +24,9 @@ function Home() {
                         </p>
                         <div className="absolute bottom-0 right-4 ">
                             <div className="inline-block">
-                                <Link to="/resume" className="action-btn" href="mailto:reply@katsuya.co?subject=Re: [Hi there]">
+                                <a href="https://drive.google.com/file/d/1hZVi1QKysHm6gSdVz0uJRsUPZbHabA3n/view?usp=sharing" className="action-btn" >
                                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clipRule="evenodd" /></svg>
-                                </Link>
+                                </a>
                             </div>
                             <div className="inline-block">
                                 <a className="action-btn" href="mailto:reply@katsuya.co?subject=Re: [Hi there]">
