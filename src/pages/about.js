a43823547132210b23bd46fe9ac87bf003d45981
katsuyaw <kw@kmail.me>
2021-06-04 22:26:31 -0700

    Edit text

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,13 @@ function About() {
         <>
             <div className="h-full grid justify-center">
                 <div className="text-base tracking-wider leading-relax sm:tracking-wider sm:leading-relax mx-12 sm:mx-64 mt-16 sm:text-xl">
-                    I love learning new things. <br />
+                    
                     I was born and raised in Japan and spent 4 years as a pre-med student before came to the US.
 I wrote my first program in 2020 and so far I have learned C/C++, Java, HTML/CSS and am learning ReactJS.
 
                 <br /><br />
 
-                I enjoy designing. <br />
-                I believe in the power of minimalism. Making a good
-                elaborate design is difficult but making it simple
-                is much more difficult. <br />
-                Since the day I learned that I fell in love with the process.
+                
                 {/* And I really enjoy what I am doing :) */}
 
                 </div>
