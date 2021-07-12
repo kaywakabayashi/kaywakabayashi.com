6a8438d8c1e159e3de183a9c34cfdc606c38e352
katsuyaw <kw@kmail.me>
2021-07-11 23:02:15 -0700

    Create component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import { Switch, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Home from './pages';
-import About from './pages/about';
-import Projects from './pages/projects';
-import Dropdown from './components/Dropdown';
-import Navbar from './components/Navbar';
+import React, { useState, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./pages";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Dropdown from "./components/Dropdown";
+import Navbar from "./components/Navbar";
+import Window from "./pages/window";
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles, GlobalStyle } from "./themes.js";
-
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
-    setIsOpen(!isOpen)
+    setIsOpen(!isOpen);
   };
 
   useEffect(() => {
@@ -23,21 +23,21 @@ function App() {
       }
     };
 
-    window.addEventListener('resize', hideMenu)
+    window.addEventListener("resize", hideMenu);
 
     return () => {
-      window.removeEventListener('resize', hideMenu)
-    }
-  })
+      window.removeEventListener("resize", hideMenu);
+    };
+  });
 
   const StyledApp = styled.div`
     color: ${(props) => props.theme.fontColor};
-  `
+  `;
 
-  const [theme, setTheme] = useState("light")
+  const [theme, setTheme] = useState("light");
 
   const themeToggler = () => {
-    theme === 'light' ? setTheme("dark") : setTheme("light")
+    theme === "light" ? setTheme("dark") : setTheme("light");
   };
 
   return (
@@ -46,13 +46,17 @@ function App() {
         <StyledApp>
           <GlobalStyles />
 
-
           <Navbar toggle={toggle} />
-          <Dropdown isOpen={isOpen} toggle={toggle} themeToggler={themeToggler} />
+          <Dropdown
+            isOpen={isOpen}
+            toggle={toggle}
+            themeToggler={themeToggler}
+          />
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
             <Route path="/projects" component={Projects} />
+            <Route path="/window" component={Window} />
           </Switch>
         </StyledApp>
       </ThemeProvider>
